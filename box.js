492f0fc3fec41562ace6f04818c3f18710af7e9c
Eduardo Pagliaroni <dudupagliaroni@gmail.com>
2021-02-22 18:43:58 -0300

    initial commit

---
+++ box.js
@@ -0,0 +1,21 @@
+function Box(x, y, w, h) {
+  this.body = Bodies.rectangle(x, y, w, h);
+  this.w = w;
+  this.h = h;
+  World.add(world, this.body);
+
+  this.show = function () {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    
+    
+
+    push();
+    fill(0,255,0);
+    //noStroke();
+    translate(pos.x, pos.y);
+    rotate(angle)
+    rect(0, 0, this.w, this.h);
+    pop();
+  };
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var boxes = [];
+var ground;
+
 function setup() {
+  background(255);
   createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  var options = {
+    isStatic: true,
+  };
+
+  ground = Bodies.rectangle(200, 385, width, 30, options);
+  World.add(world, ground);
+}
+
+function mousePressed() {
+  boxes.push(new Box(mouseX, mouseY, 20, 20));
 }
 
 function draw() {
   background(220);
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
+  fill(255, 0, 0);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width, 30);
 }
